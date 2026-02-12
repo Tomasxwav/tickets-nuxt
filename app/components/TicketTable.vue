b42cff3285602c204fb6276e661aa1cf518c7fe7
Tomasxwav <tomas_everardo9@hotmail.com>
2026-02-11 21:04:18 -0600

    feat: add TicketTable component with filtering functionality

---
+++ app/components/TicketTable.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="flex w-full justify-end mb-4">
+      <UInput v-model="globalFilter" class="w-sm" placeholder="Buscar tickets..." />
+    </div>
+    <UTable
+      ref="table"
+      v-model:global-filter="globalFilter"
+      :data="tickets"
+      :columns="columns"
+      class="flex-1"
+    />
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import { columns } from '~/constants/ticketsColumns';
+import type { Ticket } from '~/types/tickets';
+
+const props = defineProps<{
+  initialTickets: Ticket[]
+}>()
+
+
+const globalFilter = ref('')
+const tickets = ref<Ticket[]>(props.initialTickets)
+
+
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,5 @@
 <template>
   <div>
-    <div class="flex w-full justify-end mb-4">
-      <UInput v-model="globalFilter" class="w-sm" placeholder="Buscar tickets..." />
-    </div>
-
     <div v-if="loading" class="flex items-center justify-center py-12">
       <div class="text-center">
         <div class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"/>
@@ -11,24 +7,42 @@
       </div>
     </div>
 
-    <UTable
+    <UTabs 
       v-else
-      ref="table"
-      v-model:global-filter="globalFilter"
-      :data="tickets"
-      :columns="columns"
-      class="flex-1"
-    />
-  </div>
+      color="neutral"
+      variant="link"
+      :items="tabItems"
+      :ui="{
+        label: 'cursor-pointer'
+      }"
+    >
+      <template #board>
+        <p>pa ahorita </p>
+      </template>
 
+      <template #list>
+        <TicketTable :initial-tickets="tickets" class="my-8"/>
+      </template>
+    </UTabs>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { columns } from '~/constants/ticketsColumns';
 import { fetchTickets } from '~/mocks/tickets';
 import type { Ticket } from '~/types/tickets';
+import type { TabsItem } from '@nuxt/ui'
 
-const globalFilter = ref('')
+const tabItems = ref<TabsItem[]>([
+  {
+    label: 'Tablero',
+    slot: 'board'
+  },
+  {
+    label: 'Lista',
+    slot: 'list'
+  }
+])
+
 const loading = ref(true)
 const tickets = ref<Ticket[]>([])
 
