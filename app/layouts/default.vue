e3fd2a78c0a0101d2a7bf938bb345fef7d2c5f52
Tomasxwav <tomas_everardo9@hotmail.com>
2026-02-11 23:00:00 -0600

    feat: update page header title and description in default layout for clarity

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <UPage class="w-screen min-h-screen overflow-x-hidden max-w-screen">
     <UPageHeader 
-      title="Titulo"
-      description="Subtitulo"
+      title="Seguimiento de tickets"
+      description="Sistema de seguimiento de tickets para gestionar tareas y proyectos de manera eficiente."
       :ui="{
         container: 'mx-10 md:px-20 lg:px-40 2xl:px-80'
       }"
